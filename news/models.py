d9565398d9c19bea1a98758215629561413459de
ghigbie <gehigbie@gmail.com>
2019-07-29 21:31:20 -0600

    Add str to model

--- news/models.py
+++ news/models.py
@@ -9,3 +9,7 @@ class Article(models.Model):
     publication_date = models.DateField()
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author} {self.title}"
