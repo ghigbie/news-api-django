98acf3585ddacc4c0cf4d8fec5092a2c4bb3673e
ghigbie <gehigbie@gmail.com>
2019-07-29 21:29:22 -0600

    Add new article model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=200)
+    body = models.TextField(max_length=200)
+    location = models.CharField(max_length=120)
+    publication_date = models.DateField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
